1f7f18b7310628a2b0ef42decaa82d7d5ab27d82
WhitebirdStrike <144002849+WhitebirdStrike@users.noreply.github.com>
2023-10-11 16:03:09 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="home">
+    <h2>{{ appTitle }}</h2>
+
+    <h3>{{ counterData.title }}</h3>
+    <div>
+      home
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
+    </div>
+
+    <p>This counter is {{ oddOrEven }}</p>
+
+    <div class="edit">
+      <h4>Edit counter title</h4>
+      <input v-model="counterData.title" type="text" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {
+  reactive,
+  computed,
+  watch,
+  onBeforeMount,
+  onMounted,
+  onBeforeUnmount,
+  onUnmounted,
+  onActivated,
+  onDeactivated,
+} from "vue";
+
+const appTitle = "My Amazing Counter App";
+
+//const counter = ref(0);
+//const counterTitle = ref("My Counter");
+
+const counterData = reactive({
+  count: 0,
+  title: "My Counter",
+});
+
+watch(
+  () => counterData.count,
+  (newCount) => {
+    if (newCount === 20) {
+      alert("Hahaxdxdxdxdxdxdxddxdxdxdxdxdxdxdxdxdxd");
+    }
+    console.log("newCount", newCount);
+  }
+);
+
+const oddOrEven = computed(() => {
+  if (counterData.count % 2 === 0) return "even";
+  return "odd";
+});
+
+const increaseCounter = (amount, e) => {
+  counterData.count += amount;
+};
+
+const decreaseCounter = (amount) => {
+  counterData.count -= amount;
+};
+
+onBeforeMount(() => {
+  console.log("onBeforeMount");
+});
+onMounted(() => {
+  console.log("onMounted");
+});
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount");
+});
+onUnmounted(() => {
+  console.log("onUnmounted");
+});
+onActivated(() => {
+  console.log("onUnmounted");
+});
+onDeactivated(() => {
+  console.log("onDeactivated");
+});
+</script>
+<!--
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  computed: {
+    myComputedProperty() {
+      return 'my result'
+    }
+  },
+  watch: {
+    count(newCount, oldCount) {
+      if (newCount == 20) alert('20 pingas')
+    }
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  unmounted() {
+    console.log('unmounted')
+  }
+}
+
+</script>
+-->
+<!--
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const counter = ref(0);
+
+    const increaseCounter = () => {
+      counter.value++;
+    };
+
+    const decreaseCounter = () => {
+      counter.value--;
+    };
+
+    return {
+      counter,
+      decreaseCounter,
+      increaseCounter,
+    };
+  },
+};
+</script>
+-->
+
+<!--
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+    decreaseCounter() {
+      this.counter--;
+    },
+  },
+};
+</script>
+-->
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+
+.edit {
+  margin-top: 60px;
+}
+</style>
